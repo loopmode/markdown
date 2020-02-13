78ffa0046185c241d2b0b672e6c78c2633aa35a7
Jovica Aleksic <jovica.aleksic@etecture.de>
2020-02-13 17:09:05 +0100

    chore: update @codeblock

--- packages/markdown/src/Markdown.http.js
+++ packages/markdown/src/Markdown.http.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { setAutoload } from '@codeblock/core';
+import { setAutoload, getAutoload } from '@codeblock/core';
 import emptyLanguageProvider from '@codeblock/languages/lib/empty';
 import { createHttpThemeProvider } from '@codeblock/themes/lib/utils/create-http-provider';
 
 import Markdown from './Markdown';
 import { propTypes, defaultProps } from './props';
+import { useHTTPProvider } from '@codeblock/react/lib/hooks';
 
 MarkdownHTTP.propTypes = {
     prismPath: PropTypes.string.isRequired,
@@ -13,16 +14,11 @@ MarkdownHTTP.propTypes = {
 };
 MarkdownHTTP.defaultProps = defaultProps;
 export default function MarkdownHTTP(props) {
-    React.useEffect(() => {
-        setAutoload(props.prismPath);
-    }, [props.prismPath]);
+    const httpProviders = useHTTPProvider(props.prismPath);
 
-    const httpProviders = React.useMemo(() => {
-        return {
-            languages: emptyLanguageProvider,
-            themes: createHttpThemeProvider(props.prismPath)
-        };
-    }, [props.prismPath]);
-
-    return <Markdown {...props} codeblockProviders={httpProviders} />;
+    return (
+        httpProviders && (
+            <Markdown {...props} codeblockProviders={httpProviders} />
+        )
+    );
 }

c4eb77b59b3bf909173ca050a65af74c9e7fb754
Jovica Aleksic <jovica.aleksic@etecture.de>
2020-02-14 16:47:45 +0100

    Updates

--- precache-manifest.d328f5df4a809f2eca73b0758bcc6874.js
+++ precache-manifest.d328f5df4a809f2eca73b0758bcc6874.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
-    "revision": "b4e5ae2983e9ec4d219a",
+    "revision": "fe4106ce7be083ca8eab",
     "url": "/markdown/static/css/main.53aebc17.chunk.css"
   },
   {
-    "revision": "b4e5ae2983e9ec4d219a",
-    "url": "/markdown/static/js/main.e6e37ceb.chunk.js"
+    "revision": "fe4106ce7be083ca8eab",
+    "url": "/markdown/static/js/main.640af3f1.chunk.js"
   },
   {
     "revision": "1ef3ee70a0d1f62d02c7",
     "url": "/markdown/static/js/runtime~main.7d448bfc.js"
   },
   {
-    "revision": "a5d5d4ef4214503edd3f",
+    "revision": "7e34ce97acff70463d6a",
     "url": "/markdown/static/css/2.7c02b199.chunk.css"
   },
   {
-    "revision": "a5d5d4ef4214503edd3f",
-    "url": "/markdown/static/js/2.2437c7ee.chunk.js"
+    "revision": "7e34ce97acff70463d6a",
+    "url": "/markdown/static/js/2.20db838c.chunk.js"
   },
   {
-    "revision": "ca114e3818669c44f278a1c9d340ebf6",
+    "revision": "6323371a812cddf307f5a529a4ad4c96",
     "url": "/markdown/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/markdown/precache-manifest.a21f52411dc28565dd75a49b0ce06740.js"
+  "/markdown/precache-manifest.d328f5df4a809f2eca73b0758bcc6874.js"
 );
 
 workbox.clientsClaim();
